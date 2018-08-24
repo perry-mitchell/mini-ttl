7c3c96f195c089bd4e111654dc5f13ea02fe6fd2
Perry Mitchell <perry@perrymitchell.net>
2018-08-24 18:45:47 +0300

    Add travis config and integration tests

--- test/unit/index.spec.js
+++ test/unit/index.spec.js
@@ -1,4 +1,3 @@
-const sleep = require("sleep-promise");
 const TTLValue = require("../../source/index.js");
 
 describe("TTLValue", function() {
@@ -6,20 +5,6 @@ describe("TTLValue", function() {
         expect(() => {
             new TTLValue("abc");
         }).to.not.throw();
-    });
-
-    it("expires values", function() {
-        const spy1 = sinon.spy();
-        const spy2 = sinon.spy();
-        const val1 = new TTLValue(5, 100);
-        const val2 = new TTLValue(5, 200);
-        val1.on("valueExpired", spy1);
-        val2.on("valueExpired", spy2);
-        return sleep(300).then(() => {
-            expect(spy1.calledBefore(spy2)).to.be.true;
-            expect(val1.value).to.be.null;
-            expect(val2.value).to.be.null;
-        });
     });
 
     describe("get:value", function() {
